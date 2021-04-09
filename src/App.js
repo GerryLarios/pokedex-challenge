6da61c0b1e9ad695f3d3a3fe4b73049aa0930f0c
Gerardo Larios <gerardo.larios69@gmail.com>
2021-04-09 17:58:28 -0500

    [ADD] history and error handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Pokemon from './pokemon'
 
 const URL = 'https://pokeapi.co/api/v2/pokemon/'
 
+const pokemonParser = pokemon => ({
+  image: pokemon.sprites.front_default,
+  name: pokemon.name,
+  id: pokemon.id
+})
+
 const App = () => {
   const [pokemonTextInput, setPokemonTextInput] = useState('')
+  const [error, setError] = useState(false)
   const [pokemon, setPokemon] = useState()
   const [pokemonStore, setPokemonStore] = useState([])
+  const [pokemonHistory, setPokemonHistory] = useState([])
 
   const provider = pokemonString => `${URL}${pokemonString}`
 
   const handlePokemonTextInput = e => {
-    setPokemonTextInput(e.target.value)
+    setPokemonTextInput(e.target.value.toLowerCase())
   }
 
   const handleSearchPokemon = async () => {
-    const response = await fetch(provider(pokemonTextInput))
-    const result = await response.json()
-    setPokemon(result)
+    try {
+      const history = pokemonHistory.find(({ name }) => name === pokemonTextInput)
+      if (history) {
+        setPokemon(history)
+      } else {
+        const response = await fetch(provider(pokemonTextInput))
+        const result = await response.json()
+        const pkmn = pokemonParser(result)
+        setPokemonHistory(prev => [...prev, pkmn])
+        setPokemon(pkmn)
+      }
+    } catch (error) {
+      console.error(error)
+      setError(true)
+    }
   }
 
   const handleAddToStore = () => {
@@ -27,6 +47,10 @@ const App = () => {
   const handleRemoveFromStore = pokemonId => {
     setPokemonStore(prev => prev.filter(({ id }) => id !== pokemonId))
   }
+
+  useEffect(() => {
+    if (error) setTimeout(() => setError(false), 5000)
+  }, [error])
 
   return (
     <>
@@ -39,24 +63,25 @@ const App = () => {
       <section>
         {pokemon && (
           <Pokemon
-            image={pokemon.sprites.front_default}
-            name={pokemon.name}
             id={pokemon.id}
+            name={pokemon.name}
+            image={pokemon.image}
             onClick={handleAddToStore}
             disabled={pokemonStore.length >= 6}
             btnText="Catch"
           />
         )}
+        {error && <p>[ERROR] Unexpected input</p>}
         <hr />
         {pokemonStore.length === 0 ? (
           <p>Your party is empty</p>
         ) : (
           pokemonStore.map(pkmn => (
             <Pokemon
-              key={pkmn.id}
-              image={pkmn.sprites.front_default}
-              name={pkmn.name}
               id={pkmn.id}
+              key={pkmn.id}
+              name={pkmn.name}
+              image={pkmn.image}
               onClick={() => handleRemoveFromStore(pkmn.id)}
               btnText="Delete"
             />
